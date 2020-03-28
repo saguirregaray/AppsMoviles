88629cb88ff4f260f4cc7a2236ba83605851ed15
saguirregaray <saguirregaray@correo.um.edu.uy>
2020-03-28 16:31:39 -0300

    Facebook excercise

---
+++ web/run.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+var app = express();
+var port = process.env.PORT || 3000;
+app.use(serveStatic(path.join(__dirname, 'public')));
+app.listen(port,  function () {  	
+    console.log('listening on port:', port);
+    }); 
