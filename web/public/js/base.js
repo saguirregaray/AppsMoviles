8c57928971564fe4e48530694d807da697b17ec8
saguirregaray <saguirregaray@correo.um.edu.uy>
2020-05-08 22:41:57 -0300

    Change values with form

--- web/public/js/base.js
+++ web/public/js/base.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+    $("#form").removeClass("active").hide();
+
     $.ajax({
         type: 'GET',
         url: 'http://localhost:3000/person/findAll',
@@ -6,15 +9,38 @@ $(document).ready(function () {
         dataType: 'json',
         success: function (data) {
             console.log(data);
-            $('#result').text(JSON.stringify(data));
+            data.forEach(person => {
+                $("#people").append("<li id='" +person._id + "'><a>" + person.nombre + "</a></li>");
+
+                $("#" + person._id).click(function () {
+                    $('#people').removeClass("active").hide();
+                    $("#form").addClass("active").show();
+                    $("#personId").attr("value",person._id);
+                   
+                });
+            });  
         },
-        error: function (response) {
+        error: function (response) {s
             console.log("Request Failed");
         }
     });
 
+
 })
 
 
+function processForm() {
 
-//open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security
+        $.ajax({
+            url: 'http://localhost:3000/person/update/' +  $("#personId").attr('value') ,
+            dataType: 'text',
+            type: 'put',
+            contentType: 'application/x-www-form-urlencoded',
+            data: {"nombre": $("input#nombre").val(), "apellido":$("input#apellido").val(), "telefono": $("input#telefono").val()},
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+
+}
+
